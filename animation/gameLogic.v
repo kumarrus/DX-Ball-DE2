f8e5c9c08c38faec0805a7a344f65eb87fc760ee
danieldoan <danieldoan@outlook.com>
2013-11-14 19:33:26 -0500

    Update gameLogic.v

--- animation/gameLogic.v
+++ animation/gameLogic.v
@@ -1,5 +1,7 @@
 module gameLogic
 	(
+		moveLeft,
+		moveRight,
 		clk,
 		newX,
 		newY,
@@ -8,29 +10,32 @@ module gameLogic
 		sizeX,
 		sizeY,
 		startPlot
+		object
 	);
 	
-	param ballCyclesToUpdate = 5000000;
-	param paddleCyclesToUpdate = 2500000;
-	param ball_Radius = 2;
-	param maxX = 159;
-	param maxY = 119;
-	param paddleLength = 16;
-	param ballObj = 2'b00;
-	param paddleObj = 2'b01;
-	param blockObj = 2'b10;
-	param noObj = 2'b11;
+	parameter ballCyclesToUpdate = 5000000;
+	parameter paddleCyclesToUpdate = 2500000;
+	parameter ball_Radius = 2;
+	parameter maxX = 159;
+	parameter maxY = 119;
+	parameter paddleLength = 16;
+	parameter ballObj = 2'b00;
+	parameter paddleObj = 2'b01;
+	parameter blockObj = 2'b10;
+	parameter noObj = 2'b11;
 	
 //------------Input Ports--------------
 	input clk;
+	input moveLeft;
+	input moveRight;
 //----------Output Ports--------------
 	
-	output [7:0] newX;
-	output [6:0] newY;
-	output [7:0] oldX;
-	output [6:0] oldY;
-	output [7:0] sizeX;
-	output [6:0] sizeY;
+	output reg [7:0] newX;
+	output reg [6:0] newY;
+	output reg [7:0] oldX;
+	output reg [6:0] oldY;
+	output reg [7:0] sizeX;
+	output reg [6:0] sizeY;
 	output reg [1:0] object;
 	
 	output reg startPlot;
@@ -40,82 +45,86 @@ module gameLogic
 	reg [6:0] V_y = 7'b1; // Velocity y
 	reg RIGHT = 1'b1;
 	reg DOWN = 1'b1;
-	reg [7:0] new_posX = 8'b00110011; // Start x coordinate
-	reg [6:0] new_posY = 7'b0000100; // Start y coordinate
+	reg [7:0] newPosX = 8'b00110011; // Start x coordinate
+	reg [6:0] newPosY = 7'b0000100; // Start y coordinate
+	reg [7:0] oldPosX;
+	reg [6:0] oldPosY;
 	reg [7:0] oldPaddleX;
 	reg [7:0] paddleX = 'd100;
 	
-
+	/* Send proper values to mux */
 	always @ (*) begin
+		
+		if (object == ballObj) begin
+			 newX = newPosX;
+			 newY = newPosY;
+			 oldX = oldPosX;
+			 oldY = oldPosY;
+			 sizeX = 2 * ball_Radius;
+			 sizeY = 2 * ball_Radius;
+		end else if (object == paddleObj) begin
+			 newX = paddleX;
+			 newY = 7'b1111101;
+			 oldX = oldPaddleX;
+			 oldY = 7'b1111101;
+			 sizeX = paddleLength;
+			 sizeY = 1'b1;
+		end else begin
+			 newX = 8'b0;
+			 newY = 7'b1111101;
+			 oldX = 8'b0;
+			 oldY = 7'b1111101;
+			 sizeX = 8'b0;
+			 sizeY = 7'b0;	
+		end
 
-	if (object == ballObj) begin
-		assign newX = new_posX;
-		assign newY = new_posY;
-		assign oldX = old_posX;
-		assign oldY = old_posY;
-		assign sizeX = ball_Radius;
-		assign sizeY = ball_Radius;
-	end else if (object == paddleObj) begin
-		assign newX = paddleX;
-		assign newY = 7'b0000010;
-		assign oldX = oldPaddlEX;
-		assign oldY = 7'b0000010;
-		assign sizeX = paddleLength;
-		assign sizeY = 1'b1;
-	end else begin
-		assign newX = paddleX;
-		assign newY = 7'b0000010;
-		assign oldX = oldPaddleX;
-		assign oldY = 7'b0000010;
-		assign sizeX = paddleLength;
-		assign sizeY = 1'b1;
+
+		//CLOCK
+	always@(posedge clk) begin
 	
-	end
-
-
-	always@(posedge clk)
-	begin
-		//startPlot <= 1'b1;
-		if(count == ballCyclesToUpdate) //approx 1/60th of a second
-			begin
+		//IF WE CAN UPDATE BALL
+		if(count == ballCyclesToUpdate) begin
 				object = 2'b00;
 				count = 0;
 				
-				/* UPDATE */
-				if((new_posX) >= (159-4))
+				if((newPosX) >= (159-4)) //collide with right wall
 					RIGHT <= 1'b0;
-				if((new_posX) <= 1)
+				if((newPosX) <= 1) //collide with left wall
 					RIGHT <= 1'b1;
-				if((new_posY) >= (119-4)) begin
-					DOWN <= 1'b0;
-					if ( ((new_posX + ball_Radius) > paddleX) && ((new_posX + ball_Radius) < paddleX) )
-					else if ( ((new_posX + 2 * ball_Radius) > paddleX) && RIGHT)
-						RIGHT <= ~RIGHT;
-					else if ( (new_posX < (paddleX + paddleLength)) && ~RIGHT)
-						RIGHT <= ~RIGHT;
-				end
-				if((new_posY) <= 1) begin
+					
+				if((newPosY) >= (119-4)) begin //collide with bottom
+					if ( ((newPosX + ball_Radius) > paddleX) && ((newPosX + ball_Radius) < paddleX) ) begin //inside paddle
+						DOWN <= 1'b0;
+					end else if ( ((newPosX + 2 * ball_Radius) > paddleX) && RIGHT)) begin //left edge
+						DOWN <= 1'b0;
+					end else if ( (newPosX < (paddleX + paddleLength)) && ~RIGHT) begin //right edge
+						DOWN <= 1'b0;
+					end
+				end //end collide with bottom
+				
+				if((newPosY) <= 1) //collide with top
 					DOWN <= 1'b1;
+				
 				if(RIGHT) begin
-					old_posX <= new_PosX;
-					new_posX <= new_posX + V_x;
+					oldPosX <= newPosX;
+					newPosX <= newPosX + V_x;
 				end else begin
-					old_PosX <= new_PosX;
-					new_posX <= new_posX - V_x;
+					oldPosX <= newPosX;
+					newPosX <= newPosX - V_x;
 				if(DOWN) begin
-					old_posY <= new_posY;
-					new_posY <= new_posY + V_y;
+					oldPosY <= newPosY;
+					newPosY <= newPosY + V_y;
 				end else begin
-					old_posY <= new_posY;
-					new_posY <= new_posY - V_y;
+					oldPosY <= newPosY;
+					newPosY <= newPosY - V_y;
 				end
 				
 				startPlot <= 1'b1;
 			end
+		/*
 		else if (count == paddleCyclesToUpdate)
 			begin
 				object = 2'b01;
-				count = 0;
 				if (moveLeft) begin
 					if (!(paddleX == 0)) begin
 						oldPaddleX <= paddleX;
@@ -129,10 +138,11 @@ module gameLogic
 				end
 				startPlot <= 1'b1;
 			end
+		*/
 		else 
 			begin
 				startPlot <= 1'b0;
-				object = noObj;
+				object = 2'b00;
 				count = count + 1;
 			end
 	end
